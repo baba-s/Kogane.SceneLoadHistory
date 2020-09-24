b85adbbe7b35e684c80bb7440699b17d425a29f1
baba_s <shotababa1206@gmail.com>
2020-09-24 19:41:14 +0900

    first commit

---
+++ Scripts/SceneLoadHistoryData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Kogane
+{
+	public sealed class SceneLoadHistoryData
+	{
+		private readonly Scene         m_scene;
+		private readonly LoadSceneMode m_mode;
+
+		public string        Path          => m_scene.path;
+		public string        Name          => m_scene.name;
+		public int           BuildIndex    => m_scene.buildIndex;
+		public LoadSceneMode LoadSceneMode => m_mode;
+
+		internal SceneLoadHistoryData( Scene scene, LoadSceneMode mode )
+		{
+			m_scene = scene;
+			m_mode  = mode;
+		}
+	}
+}
